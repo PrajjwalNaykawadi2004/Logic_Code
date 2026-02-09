47d116ae6f5bdb5f44d3b6454a7ef0e03e0f1014
Prajjwal Naykawadi <naykawadiprajjwal@gmail.com>
2026-02-09 11:01:27 +0000

    Add files via upload

---
+++ Day_28/program680.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main()
+{
+    printf("PID of running process : %d\n",getpid());
+
+    printf("PID of parent process : %d\n",getppid());
+
+    return 0;
+}
+
