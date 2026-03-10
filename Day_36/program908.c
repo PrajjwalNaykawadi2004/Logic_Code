50f2da1444db06aabfda464e73667ac298b3bae9
Prajjwal Naykawadi <naykawadiprajjwal@gmail.com>
2026-03-10 14:46:57 +0530

    Add files via upload

---
+++ Day_36/program908.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+int main()
+{
+	printf("Home directory : %s\n",getenv("HOME"));
+	printf("Shell used : %s\n",getenv("SHELL"));
+	
+	return 0;
+}
